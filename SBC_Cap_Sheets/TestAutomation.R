08dd32ed3ebcb845f67040211e540b7c5c184534
McCade Pearson <mppackersfan97@gmail.com>
2025-08-08 22:18:51 -0600

    test 5

--- SBC_Cap_Sheets/TestAutomation.R
+++ SBC_Cap_Sheets/TestAutomation.R
@@ -1,7 +1,7 @@
 library(httr)
 library(tidyverse)
 
-Schedule <- readRDS("SBC_Cap_Sheets/SBC_Bref/Games.rds")
+Schedule <- readRDS("SBC_Bref/Games.rds")
 
 discord_webhook <- "https://discord.com/api/webhooks/1396720354034057326/IBOVLxOQ4xKmk2VEAX6fBgWRrsc6SZJSlMlcQVF0LovCM9wpqeivQqBMdTf4kNZHYmyv"
 
