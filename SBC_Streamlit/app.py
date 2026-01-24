56e2110719e4929fe0ad820bae0cdfaf9cd282cf
McCade Pearson <mppackersfan97@gmail.com>
2026-01-24 16:06:12 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -82,7 +82,7 @@ with tab1:
         active_player_df = active_players(df, pics, SelectedTeam)
         active_player_df = (active_player_df.style
             .apply(style_salaries, axis=1, type_colors=type_colors)
-            .format({c: "${:,.0f}" for c in df.columns if re.match(r"\d{4}", c)}))
+            .format({c: "${:,.0f}" for c in active_player_df.columns if re.match(r"\d{4}", c)}))
         st.dataframe(active_player_df, width = "stretch", height = "content", row_height = 50, hide_index=True, placeholder="—", column_order=("Picture_Online", "Player", "Y2026", "Y2027", "Y2028", "Y2029", "Y2030","Y2031", "Y2032"), column_config={"Picture_Online": st.column_config.ImageColumn("Picture_Online")})
 
 with tab2:
