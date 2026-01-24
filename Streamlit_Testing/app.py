e2aa4ce806b078cf5668a6e823ad33e99ade3988
McCade Pearson <mppackersfan97@gmail.com>
2026-01-23 20:17:35 -0700

    Update app.py

--- Streamlit_Testing/app.py
+++ Streamlit_Testing/app.py
@@ -105,7 +105,7 @@ else:
 
 col1, col2 = st.columns([1, 4])
 with col1:
-    st.metric(label = "Players", value = 17, delta = 10, delta_color = "off", help = "Number of Players on Active and Inactive Roster", border = True, format = "dollar")
+    st.metric(label = "Players", value = 17, delta = 10, delta_color = "off", help = "Number of Players on Active and Inactive Roster", border = True, format = "plain", delta_arrow = "off")
     st.metric(label = "Cap Total", value = 244489135, delta = -89842135, delta_color = "normal", help = "Salary Cap Hit and Cap Space", border = True, format = "dollar")
     st.metric(label = "Tax Total", value = 210917997, delta = -23022997, delta_color = "normal", help = "Tax Hit and Space", border = True, format = "dollar")
     st.metric(label = "Apron Space", value = None, help = "Tax Hit and Space", border = True, format = "dollar")
