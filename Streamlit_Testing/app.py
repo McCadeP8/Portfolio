94b65aa488c32d1b071ebb7ce618b527080e73e5
McCade Pearson <mppackersfan97@gmail.com>
2026-01-23 19:01:17 -0700

    Update app.py

--- Streamlit_Testing/app.py
+++ Streamlit_Testing/app.py
@@ -10,7 +10,7 @@ st.title("CSV Data Viewer")
 
 col1, col2 = st.columns([4, 1])
 with col1:
-    st.header("Vegas Blackjack"")
+    st.header("Vegas Blackjack")
 with col2:
     st.image("https://pbs.twimg.com/media/Fxam4dlaIAIKnBb?format=png&name=4096x4096", width=500)
 
