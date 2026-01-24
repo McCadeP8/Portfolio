dc7dbe43833e30b9894dbaecb6614830b841f9fb
McCade Pearson <mppackersfan97@gmail.com>
2026-01-24 16:14:57 -0700

    Comment out styling for active player DataFrame
    The code that applies custom styling and formatting to the active player DataFrame has been commented out, possibly for debugging or to address display issues. The raw DataFrame is now shown in the Streamlit app.

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -80,9 +80,9 @@ with tab1:
     with col2:
         st.subheader("Active Players")
         active_player_df = active_players(df, pics, SelectedTeam)
-        active_player_df = (active_player_df.style
-            .apply(style_salaries, axis=1, type_colors=type_colors)
-            .format({c: "${:,.0f}" for c in active_player_df.columns if re.match(r"\d{4}", c)}))
+        #active_player_df = (active_player_df.style
+        #    .apply(style_salaries, axis=1, type_colors=type_colors)
+        #    .format({c: "${:,.0f}" for c in active_player_df.columns if re.match(r"\d{4}", c)}))
         st.dataframe(active_player_df, width = "stretch", height = "content", row_height = 50, hide_index=True, placeholder="—", column_order=(" ", "Player", "2026", "2027", "2028", "2029", "2030","2031", "2032"), column_config={" ": st.column_config.ImageColumn(" ")})
 
 with tab2:
