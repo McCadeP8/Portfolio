5e874f158c0c75fdde8c7dd97bd5cb0b2b42cc04
McCade Pearson <mppackersfan97@gmail.com>
2026-01-24 20:50:29 -0700

    Update functions.py

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -80,7 +80,7 @@ def dead_players(df: pd.DataFrame, pics: pd.DataFrame, SelectedTeam: str) -> pd.
 def free_agent_players(df: pd.DataFrame, pics: pd.DataFrame, SelectedTeam: str) -> pd.DataFrame:
     df = df.merge(pics[['Player', 'Picture_Online']], on='Player', how='left')
     df = df[df['Team'] == SelectedTeam]
-    df = df[df['Type2026'].isin(['Unrestricted', 'Restricted'])]
+    df = df[df['Type2027'].isin(['Unrestricted', 'Restricted'])]
     year_cols = ['Y2026','Y2027','Y2028','Y2029','Y2030','Y2031','Y2032']
     type_cols_keep = ['Type2026','Type2027','Type2028','Type2029','Type2030','Type2031','Type2032']
     cols_to_keep = ['Picture_Online','Player'] + year_cols + type_cols_keep
