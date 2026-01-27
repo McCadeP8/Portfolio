66b2bf6e9986d38492ee90759a513e942dfa3b61
McCade Pearson <mppackersfan97@gmail.com>
2026-01-26 19:42:56 -0700

    Update functions.py

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -134,6 +134,8 @@ def inactive_player_n(df: pd.DataFrame, SelectedTeam: str) -> pd.DataFrame:
     df = df[df["Type" + str(current_year)].isin(['Guaranteed', 'Unguaranteed'])]
     return df.shape[0]
 
+#ABC
+
 def get_cap_total(df: pd.DataFrame, exceptions_df: pd.DataFrame, SelectedTeam: str) -> float:
     df = df[df['Team'] == SelectedTeam]
     player_total = df["Y" + str(current_year)].sum()
