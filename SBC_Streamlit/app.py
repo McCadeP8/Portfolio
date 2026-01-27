757fba4d0630f484c5e0cfcd6193739369ec447e
McCade Pearson <mppackersfan97@gmail.com>
2026-01-26 22:25:18 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -163,7 +163,7 @@ with tab3:
         active_all_df = (active_all_df.style
             .apply(lambda row: style_salaries(row, type_colors), axis=1)  
             .format({c: "${:,.0f}" for c in overseas_player_df.columns if re.match(r"\d{4}", c)}))
-        st.dataframe(active_all_df, width = "stretch", height = "content", row_height = 100, hide_index=True, placeholder="—", column_order=["Team_logo", " ", "Player"] + columns_order + ["Bird Rights"], column_config={" ": st.column_config.ImageColumn(label="", width="small"), 
+        st.dataframe(active_all_df, width = "stretch", height = "content", row_height = 50, hide_index=True, placeholder="—", column_order=["Team_logo", " ", "Player"] + columns_order + ["Bird Rights"], column_config={" ": st.column_config.ImageColumn(label="", width="small"), 
                                                                     "Team_logo": st.column_config.ImageColumn(label="", width="small")})
 
 
