e0619836b5cbb1bb7b96628c28d3b91ac94dd2b0
McCade Pearson <mppackersfan97@gmail.com>
2026-01-27 11:43:04 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -199,7 +199,7 @@ with tab3:
             retired_all_df = (retired_all_df.style
                 .apply(lambda row: style_salaries(row, type_colors), axis=1)  
                 .format({c: "${:,.0f}" for c in retired_all_df.columns if re.match(r"\d{4}", c)}))
-            st.dataframe(retired_all_df, width = "stretch", height = "content", row_height = 50, hide_index=True, placeholder="—", column_order=["Team_logo", " ", "Player"] + columns_order, column_config={" ": st.column_config.ImageColumn(label="", width="small"), "Team_logo": st.column_config.ImageColumn(label="", width="small")})
+            st.dataframe(retired_all_df, width = "stretch", height = "content", row_height = 50, hide_index=True, placeholder="—", column_order=["Team_logo", " ", "Player"], column_config={" ": st.column_config.ImageColumn(label="", width="small"), "Team_logo": st.column_config.ImageColumn(label="", width="small")})
 
     with col3:
         st.subheader("Draft Rights")
