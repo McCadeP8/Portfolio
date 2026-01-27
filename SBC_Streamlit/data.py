f39e3eafc57764e173ab871b1d9b1aa688fc3d1d
McCade Pearson <mppackersfan97@gmail.com>
2026-01-26 19:39:27 -0700

    Update data.py

--- SBC_Streamlit/data.py
+++ SBC_Streamlit/data.py
@@ -5,9 +5,9 @@ today = date.today()
 if today.month > 7 or (today.month == 7 and today.day >= 2):
     current_year = today.year + 1
 else:
-    current_year = today.year + 3
+    current_year = today.year
 
-columns_order = [str(current_year + i) for i in range(2)]
+columns_order = [str(current_year + i) for i in range(7)]
 
 
 current_salary_cap = 154647000
