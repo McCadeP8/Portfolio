525d586ad4eb9b5a70686690c17fedf51a4ffbaf
McCade Pearson <mppackersfan97@gmail.com>
2026-01-27 12:10:54 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import re as re
-from functions import get_data, get_pictures, active_players, style_salaries, overseas_players, free_agent_players, dead_players, draft_retired_players, active_player_n, inactive_player_n, get_exceptions, exception_table, get_cap_total, get_tax_total, get_base_cap, team_hard_cap, team_hard_cap_n, base_fee, amount_paid, net_fee, luxury_fee, tax_amount_calc, trade_restrictions, active_players_all, inactive_players_all, dead_players_all, draft_rights_all, retired_all, all_free_agents
+from functions import get_data, get_pictures, active_players, style_salaries, overseas_players, free_agent_players, dead_players, draft_retired_players, active_player_n, inactive_player_n, get_exceptions, exception_table, get_cap_total, get_tax_total, get_base_cap, team_hard_cap, team_hard_cap_n, base_fee, amount_paid, net_fee, luxury_fee, tax_amount_calc, trade_restrictions, active_players_all, inactive_players_all, dead_players_all, draft_rights_all, retired_all, all_free_agents, trade_restrictions_all
 from data import team_info, type_colors, current_salary_cap, current_luxury_tax, current_apron_1, current_apron_2, current_year, columns_order, year_offset
 
 df = get_data()
@@ -172,7 +172,6 @@ with tab3:
             <span style="background-color:#D9D9D9;padding:6px 20px;border-radius:5px;">&nbsp;</span> Dead
             """, unsafe_allow_html=True)
 
-
     with col2:
         active_all_df = active_players_all(df, pics)
         if active_all_df.shape[0] > 0:
@@ -203,7 +202,7 @@ with tab3:
     with col1:
         all_free_agents_df = all_free_agents(df, pics)
         if all_free_agents_df.shape[0] > 0:
-            st.subheader("Retired Rights")
+            st.subheader("Upcoming Free Agents")
             all_free_agents_df = (all_free_agents_df.style
                 .apply(lambda row: style_salaries(row, type_colors), axis=1)  
                 .format({c: "${:,.0f}" for c in all_free_agents_df.columns if re.match(r"\d{4}", c)}))
@@ -228,8 +227,13 @@ with tab3:
             st.dataframe(retired_all_df, width = "stretch", row_height = 50, hide_index=True, placeholder="—", column_order=["Team_logo", " ", "Player"], column_config={" ": st.column_config.ImageColumn(label="", width="small"), "Team_logo": st.column_config.ImageColumn(label="", width="small")})
 
     with col4:
-        st.subheader("Draft Rights")
-
+        trade_restrictins_all_df = trade_restrictions_all(df, pics)
+        if trade_restrictins_all_df.shape[0] > 0:
+            st.subheader("Trade Restrictions")
+            trade_restrictins_all_df = (trade_restrictins_all_df.style
+                .apply(lambda row: style_salaries(row, type_colors), axis=1)  
+                .format({c: "${:,.0f}" for c in trade_restrictins_all_df.columns if re.match(r"\d{4}", c)}))
+            st.dataframe(trade_restrictins_all_df, width = "stretch", row_height = 50, hide_index=True, placeholder="—", column_order=["Team_logo", " ", "Player"], column_config={" ": st.column_config.ImageColumn(label="", width="small"), "Team_logo": st.column_config.ImageColumn(label="", width="small")})
 
 with tab4:
     st.markdown("This section is under construction.")
