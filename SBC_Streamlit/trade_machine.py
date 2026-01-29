19022ad9fe9ed264df25570ecb44c37bd9a3a72b
McCade Pearson <mppackersfan97@gmail.com>
2026-01-29 08:56:00 -0700

    Update trade_machine.py

--- SBC_Streamlit/trade_machine.py
+++ SBC_Streamlit/trade_machine.py
@@ -3,7 +3,7 @@ import streamlit as st
 import math as math
 from data import current_salary_cap, current_luxury_tax, current_apron_1, current_apron_2, tax_bracket_increment, league_ratio, columns_order, current_year, year_offset, team_info
 
-def tradeable_players(df: pd.DataFrame, SelectedTeam: str) -> list[str]:
+def tradeable_players_out(df: pd.DataFrame, SelectedTeam: str) -> list[str]:
     df = df[df['Team'] == SelectedTeam]
     df = df[df['Trade.Restriction'].isna()]
     df = df[['Player']]
@@ -11,3 +11,10 @@ def tradeable_players(df: pd.DataFrame, SelectedTeam: str) -> list[str]:
     df_list = df['Player'].tolist()
     return df_list
 
+def tradeable_players_in(df: pd.DataFrame, SelectedTeam: str) -> list[str]:
+    df = df[df['Team'] != SelectedTeam]
+    df = df[df['Trade.Restriction'].isna()]
+    df = df[['Player']]
+    df = df.sort_values('Player', ascending=True)
+    df_list = df['Player'].tolist()
+    return df_list
