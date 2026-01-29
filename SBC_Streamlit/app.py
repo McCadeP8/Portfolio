2f89018e74ba0bb4d240b834e9737df41b46fa5c
McCade Pearson <mppackersfan97@gmail.com>
2026-01-28 21:19:19 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -282,11 +282,11 @@ with tab5:
 with tab6:
     col1, col2 = st.columns(2)
     with col1:
-        SelectedTeam1 = st.multiselect("Select your Players:", Teams, index=Teams.index("Vegas"))
-        SelectedTeam2 = st.multiselect("Select your Picks:", Teams, index=Teams.index("Vegas"))
+        SelectedTeam1 = st.multiselect("Select your Players:", Teams, default=["Vegas"])
+        SelectedTeam2 = st.multiselect("Select your Picks:", Teams, default=["Vegas"])
     with col2:
-        SelectedTeam3 = st.multiselect("Select your Players2:", Teams, index=Teams.index("Vegas"))
-        SelectedTeam4 = st.multiselect("Select your Picks2:", Teams, index=Teams.index("Vegas"))
+        SelectedTeam3 = st.multiselect("Select your Players2:", Teams, default=["Vegas"])
+        SelectedTeam4 = st.multiselect("Select your Picks2:", Teams, default=["Vegas"])
 
 with tab7:
     st.subheader("SBCFBL Introduction")
