da55622b9dcb0f1accfbcac4ac6160859522fbad
McCade Pearson <mppackersfan97@gmail.com>
2026-01-29 22:15:51 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -172,6 +172,8 @@ with tab1:
 
 with tab2:
     st.header(f"{SelectedTeam} Future Draft Picks")
+    st.divider()
+    st.header("Fully Owned Picks")
     st.dataframe(full_draft_picks(dp, SelectedTeam), width = "stretch", height = "content", row_height = 50, hide_index=True, placeholder="—", column_config={"OGTeam": st.column_config.ImageColumn(label="Slot", width="small"), "CurrentTeam": st.column_config.ImageColumn(label="Owner", width="small")})
 
 
