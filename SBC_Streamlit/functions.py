905b5f695a6ce6d50849ee660cc5aa929e2d6721
McCade Pearson <mppackersfan97@gmail.com>
2026-02-01 14:13:52 -0700

    roster check
    fantrax_roster_check

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -869,3 +869,16 @@ def fantrax_players_check(df: pd.DataFrame, ft_players: pd.DataFrame, ft_rosters
     df = df.rename(columns={'name': 'Fantrax Name'})
     df = df[['Cap Sheet Name', 'Fantrax Name']]
     return df
+
+def fantrax_roster_check(df: pd.DataFrame, ft_players: pd.DataFrame, ft_rosters: pd.DataFrame) -> pd.DataFrame:
+    df = get_data()
+    df['Player'] = df['Player'].replace(cap_sheets_to_fantrax_name_fix)
+    df = df[df['Player'] != "Minimum Salary Penalty"]
+    df = df[df['Trade.Restriction'] != "Dead"]
+    df = df[df['Trade.Restriction'] != "Banned"]
+    df = df.merge(ft_players, how='left', left_on='Player', right_on='name')
+    df = df.merge(ft_rosters, how='outer', left_on='fantraxId', right_on='id')
+    df['status2'] = df['status'].apply(lambda x: "Non-Active Players" if x == "MINORS" else "Active Players")
+    df = df[df['Type'] != df["status2"]]
+    df = df[['Type', 'Player', 'Team', 'status']]
+    return df
