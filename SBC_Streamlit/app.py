542b54be6ff9bfe8b6eea976be4c94df6236d9e6
McCade Pearson <mppackersfan97@gmail.com>
2026-01-31 20:30:36 -0700

    Update app.py

--- SBC_Streamlit/app.py
+++ SBC_Streamlit/app.py
@@ -19,7 +19,7 @@ with st.sidebar:
     text_color2 = team_info[SelectedTeam]["bg2"]
     team_logo = team_info[SelectedTeam]["logo"]
     nickname = team_info[SelectedTeam]["nickname"]
-    st.logo(team_logo, width=250)
+    st.logo(team_logo)
 
 st.markdown(
     f"""
