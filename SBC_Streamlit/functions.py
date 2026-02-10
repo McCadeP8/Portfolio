859eb75373eb91d8fce941ee75d93183a5bb3bc8
McCade Pearson <mppackersfan97@gmail.com>
2026-02-09 18:45:31 -0700

    Update functions.py

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -1178,6 +1178,7 @@ def team_with_ranks(df: pd.DataFrame, team_value: str) -> pd.DataFrame:
     out.insert(0, "Team", [team_value, "League Rank"])
     return out
 
+current_matchup = current_matchup_period()
 def team_stats_line_chart(df: pd.DataFrame, SelectedTeam: str, SelectedCategory: str) -> alt.Chart:
     df_year = df[df["Year"] == current_year]
     df_year = df[df["Period"] <= current_matchup]
