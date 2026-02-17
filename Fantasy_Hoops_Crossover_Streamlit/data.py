ca7e38afface6db85708c88c64e43a0c17e19fda
McCade Pearson <mppackersfan97@gmail.com>
2026-02-16 19:06:01 -0700

    Update data.py

--- Fantasy_Hoops_Crossover_Streamlit/data.py
+++ Fantasy_Hoops_Crossover_Streamlit/data.py
@@ -27,9 +27,6 @@ def get_logo(df: pd.DataFrame, SelectedTeam: str) -> str:
     return team_row["Logo"].iloc[0]
 
 def get_team_schedule(df: pd.DataFrame, df2: pd.DataFrame, df3: pd.DataFrame, SelectedTeam: str, Tournament: str) -> str:
-    df = get_schedule()
-    df2 = get_weekly_scores()
-    df3 = get_teams()
     df = df[df["Team"] == SelectedTeam]
     df[["Week", "Type"]] = df["Matchup"].str.split("_", n=1, expand=True)
     df = df.merge(df2, how="left", on=["Team", "Week"]).rename(columns={"Score": "Team Score"})
@@ -42,3 +39,45 @@ def get_team_schedule(df: pd.DataFrame, df2: pd.DataFrame, df3: pd.DataFrame, Se
     df = df.drop("Type", axis=1)
     return df
 
+def get_base_records(df: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    df = get_schedule()
+    df2 = get_weekly_scores()
+    df[["Week", "Type"]] = df["Matchup"].str.split("_", n=1, expand=True)
+    df = df.merge(df2, how="left", on=["Team", "Week"]).rename(columns={"Score": "Team Score"})
+    df = df.merge(df2, how="left", left_on=["Opponent", "Week"], right_on=["Team", "Week"]).rename(columns={"Score": "Opponent Score"})
+    df["Result"] = np.select([df["Team Score"] > df["Opponent Score"], df["Team Score"] < df["Opponent Score"]], ["Win", "Loss"], default="Tie")
+    df = df[~df["Opponent Score"].isna()]
+    df = df.groupby("Team_x")["Result"].value_counts().unstack(fill_value=0).reset_index()
+    df["Win %"] = (df["Win"] + 0.5 * df["Tie"]) / (df["Win"] + df["Loss"] + df["Tie"])
+    df["Record"] = np.where(df["Tie"] > 0, df["Win"].astype(str) + "-" + df["Loss"].astype(str) + "-" + df["Tie"].astype(str), df["Win"].astype(str) + "-" + df["Loss"].astype(str))
+    df = df.rename(columns={"Team_x": "Team"})
+    df = df[["Team", "Record", "Win %"]]
+    df["Rank_num"] = df["Win %"].rank(method="min", ascending=False)
+    df["Rank_num"] = df["Rank_num"].astype(int)
+    tie_counts = df.groupby("Win %")["Win %"].transform("count")
+    df["Rank"] = np.where(tie_counts > 1, "T-" + df["Rank_num"].astype(str), df["Rank_num"].astype(str))
+    df = df.drop(columns=["Rank_num"])
+    return df
+
+def get_team_record(df: pd.DataFrame, SelectedTeam: str) -> str:
+    team_row = df[df["Team"] == SelectedTeam]
+    return team_row["Record"].iloc[0]
+
+def get_team_record_rank(df: pd.DataFrame, SelectedTeam: str) -> str:
+    team_row = df[df["Team"] == SelectedTeam]
+    return team_row["Rank"].iloc[0]
+
+def get_opponent_record(df: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    df = get_schedule()
+    df3 = get_base_records(df, df2)
+
+
+def PointsPerWeek()
+
+def get_RPI()
+
+def get_SOS()
+
+def get_Weighted_Pts()
+
+def get_
