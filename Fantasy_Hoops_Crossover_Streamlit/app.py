f28d3f9a05173a0fbfe3e1af78e40a451c5c2ee4
McCade Pearson <mppackersfan97@gmail.com>
2026-02-21 14:25:06 -0700

    Update app.py

--- Fantasy_Hoops_Crossover_Streamlit/app.py
+++ Fantasy_Hoops_Crossover_Streamlit/app.py
@@ -471,7 +471,7 @@ with tab2:
         color_b  = st.color_picker("Color",  "#1D428A",  key="cb")
 
     
-    col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
+    col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
 
     with col1:
         render_scorebug(
@@ -658,6 +658,14 @@ with tab2:
             record_a=record_a, record_b=record_b,
             score_a=int(score_a), score_b=int(score_b),
             color_a=color_a,   color_b=color_b)
+    with col6:
+        render_scorebug(
+            team_a=team_a,     team_b=team_b,
+            logo_a=logo_a,     logo_b=logo_b,
+            record_a=record_a, record_b=record_b,
+            score_a=int(score_a), score_b=int(score_b),
+            color_a=color_a,   color_b=color_b)
+
 
 
     st.divider()
