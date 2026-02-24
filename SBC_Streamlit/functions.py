43d7b8d1872cfb2592908314100dc9ca525a3d05
McCade Pearson <mppackersfan97@gmail.com>
2026-02-23 20:29:35 -0700

    Update functions.py

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -1328,16 +1328,8 @@ def matchup_scoreboard(df: pd.DataFrame, SelectedTeam: str, SelectedYear: int, S
     return styled_team2
 
 def get_opponents(df: pd.DataFrame, SelectedTeam: str, SelectedYear: int, SelectedPeriod: int, Type: str) -> list:    
-    
-    df = get_all_time_schedule()
-    SelectedTeam = "Vegas"
-    SelectedYear = 2024
-    SelectedPeriod = 30
-    Type = "Regular Season"
-
     filtered = df[(df["Type"] == Type) & (df["Year"] == SelectedYear) & (df["Period"] == SelectedPeriod)].copy()
     filtered = filtered[(filtered["TeamA"].str.contains(SelectedTeam)) | (filtered["TeamB"].str.contains(SelectedTeam))]
-    full_team_to_location = {f"{loc} {info['nickname']}": loc for loc, info in team_info.items()}
     opponents = []
     for _, row in filtered.iterrows():
         if SelectedTeam in row["TeamA"]:
