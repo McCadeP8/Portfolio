a7ed8cf8d6491fd3600b064f7ca576d94e3206c2
McCade Pearson <mppackersfan97@gmail.com>
2026-02-28 14:52:24 -0700

    Update functions.py

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -1451,7 +1451,6 @@ def get_short_term_awards(df: pd.DataFrame, df2: pd.DataFrame, df3: pd.DataFrame
         return None 
     df["logo"] = df["team_name"].apply(lambda x: get_team_logo(x, team_info))
     df["conf"] = df["team_name"].apply(lambda x: get_team_conf(x, team_info))
-    df = df[df["conf"] == Conference]
     df = df[["Week", "logo", "Winner", "Picture_Online"]]
     df = df.sort_values("Week")
     return df
