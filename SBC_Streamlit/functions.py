525bc69883cb842f946735a6eceef7dad51dd339
McCade Pearson <mppackersfan97@gmail.com>
2026-02-28 14:40:16 -0700

    Update functions.py

--- SBC_Streamlit/functions.py
+++ SBC_Streamlit/functions.py
@@ -133,14 +133,16 @@ def get_draft_history() -> pd.DataFrame:
 def get_award_history() -> pd.DataFrame:
     csv_url = "https://docs.google.com/spreadsheets/d/1yQFnD0MK0cjO68_Mri6N115EmblyDW7Bza2hbY9Rerg/export?format=csv&gid=1698988928"
     df = pd.read_csv(csv_url)
-    df = df.melt(id_vars=["Award"], var_name="Year", value_name="Winner")    
+    df = df.melt(id_vars=["Award"], var_name="Year", value_name="Winner")
+    df["Year"] = df["Year"].astype(int)  
     return df
 
 @st.cache_data()
 def get_team_award_history() -> pd.DataFrame:
     csv_url = "https://docs.google.com/spreadsheets/d/1yQFnD0MK0cjO68_Mri6N115EmblyDW7Bza2hbY9Rerg/export?format=csv&gid=451021615"
     df = pd.read_csv(csv_url)
-    df = df.melt(id_vars=["Award"], var_name="Year", value_name="Winner")    
+    df = df.melt(id_vars=["Award"], var_name="Year", value_name="Winner")  
+    df["Year"] = df["Year"].astype(int)    
     return df
 
 @st.cache_data()
