414be0b221c6c0da8452ad1c5eb2760b4adbe2af
McCade Pearson <mppackersfan97@gmail.com>
2026-03-12 12:49:01 -0600

    Update app.py

--- Kirk_March_Madness/app.py
+++ Kirk_March_Madness/app.py
@@ -2,7 +2,7 @@
 #os.chdir("Kirk_March_Madness")
 
 import streamlit as st
-from data import get_projections, get_picks, calculate_expected_points, calculate_risk_score, run_simulations, calculate_finish_chances, score_simulations, plot_correct_picks
+from data import get_projections, get_picks, calculate_expected_points, calculate_risk_score, run_simulations, calculate_finish_chances, score_simulations, plot_correct_picks, compute_all_results
 
 projections = get_projections()
 picks = get_picks()
@@ -11,9 +11,9 @@ RiskScore = calculate_risk_score(projections, picks)
 sims   = run_simulations(projections, n_simulations=10000)
 scores = score_simulations(picks, sims, projections)
 finish = calculate_finish_chances(scores)
-
+AllResults = compute_all_results(picks, sims) 
 st.title("Kirk's March Madness Bracket Analysis")
 
 selected_bracket = st.selectbox('Select Bracket', picks['Bracket'].unique())
 round_name = st.selectbox('Select Round', ['All', 'R64', 'R32', 'S16', 'E8', 'F4', 'Champ'])
-plot_correct_picks(picks, sims, selected_bracket, round_name)
+plot_correct_picks(AllResults, selected_bracket, round_name)
