bc1f73a3127f1ebc6480ed34f42f1a3786e0f1d6
McCade Pearson <mppackersfan97@gmail.com>
2026-03-12 13:03:25 -0600

    Update app.py

--- Kirk_March_Madness/app.py
+++ Kirk_March_Madness/app.py
@@ -5,6 +5,11 @@ import streamlit as st
 import numpy as np
 from data import get_projections, get_picks, calculate_expected_points, calculate_risk_score, run_simulations, calculate_finish_chances, score_simulations, plot_correct_picks, compute_all_results
 
+st.set_page_config(
+    page_title = "Kir's March Madness Bracket Analysis",
+    page_icon = ":basketball:",
+    layout = "wide")
+
 projections = get_projections()
 picks = get_picks()
 expected_points = calculate_expected_points(projections, picks)
@@ -14,9 +19,6 @@ scores = score_simulations(picks, sims, projections)
 finish = calculate_finish_chances(scores)
 AllResults = compute_all_results(picks, sims)
     
-    
-
-st.title("Kirk's March Madness Bracket Analysis")
 selected_bracket = st.selectbox('Select Bracket', picks['Bracket'].unique())
 round_name = st.selectbox('Select Round', ['R64', 'R32', 'S16', 'E8', 'F4', 'Champ'])
 plot_correct_picks(AllResults, selected_bracket, round_name)
