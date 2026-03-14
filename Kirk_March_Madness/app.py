a8fff422fc847b4bd70cd7b96341e9f45a497b16
McCade Pearson <mppackersfan97@gmail.com>
2026-03-13 20:28:34 -0600

    Update app.py

--- Kirk_March_Madness/app.py
+++ Kirk_March_Madness/app.py
@@ -240,27 +240,25 @@ with tab1:
 
 with tab2:
     OverallData = update_total_expected(TotalExpected, ScoresTotal, CountsTotal, RiskScore, Picks, Finish)
-st.dataframe(
-    OverallData,
-    use_container_width=True, height="content",
-    column_config={
-        "Pred. Pts": st.column_config.NumberColumn(format="%.2f"),
-        "Pred. Games": st.column_config.NumberColumn(format="%.2f"),
-        "Total EV": st.column_config.NumberColumn(format="$%.2f"),
+    st.dataframe(
+        OverallData,
+        use_container_width=True, height="content", hide_index=True,
+        column_config={
+            "Pred. Pts": st.column_config.NumberColumn(format="%.2f"),
+            "Pred. Games": st.column_config.NumberColumn(format="%.2f"),
+            "Total EV": st.column_config.NumberColumn(format="$%.2f"),
 
-        "Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "ITM%": st.column_config.NumberColumn(format="%.2f%%"),
-        "Th Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "Fr Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "W Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "E Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "S Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "MW Win%": st.column_config.NumberColumn(format="%.2f%%"),
-        "S16 Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "ITM%": st.column_config.NumberColumn(format="%.2f%%"),
+            "Th Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "Fr Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "W Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "E Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "S Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "MW Win%": st.column_config.NumberColumn(format="%.2f%%"),
+            "S16 Win%": st.column_config.NumberColumn(format="%.2f%%"),
 
-        "Risk Score": st.column_config.NumberColumn(format="%.2f"),
-        "Downside": st.column_config.NumberColumn(format="%.2f"),
-        "Champ Risk": st.column_config.NumberColumn(format="%.2f"),
-        "Avg. Upset": st.column_config.NumberColumn(format="%.2f"),
-    }
-)
+            "Risk Score": st.column_config.NumberColumn(format="%.2f"),
+            "Downside": st.column_config.NumberColumn(format="%.2f"),
+            "Champ Risk": st.column_config.NumberColumn(format="%.2f"),
+            "Avg. Upset": st.column_config.NumberColumn(format="%.2f")})
